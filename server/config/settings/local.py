6705e60071f7344edbee868f3a661954272e9499
Joel Hillacre <joel@403forbidden.ca>
2026-06-05 09:40:44 -0600

    wip: split production and local server config

--- server/config/settings/local.py
+++ server/config/settings/local.py
@@ -4,14 +4,6 @@ DEBUG = True
 
 CSRF_COOKIE_NAME = "widget-warehouse-csrf-token"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://astute.arrai.com:8080",
-    "http://localhost:8080",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "https://astute.arrai.com:8080",
-    "http://localhost:8080",
-]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_COOKIE_SECURE = False
